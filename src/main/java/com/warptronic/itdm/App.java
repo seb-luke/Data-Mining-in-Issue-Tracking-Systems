b7dfa1708854c3db9deafd134a5d0fb755928f6b
Sebastian Luca <seb.luke@gmail.com>
2016-07-01 23:41:33 +0300

    Take the Jira parameters from call arguments

--- src/main/java/com/warptronic/itdm/App.java
+++ src/main/java/com/warptronic/itdm/App.java
@@ -10,8 +10,54 @@ import com.warptronic.itdm.jira.Request;
 public class App {
 	
 	public static void main (String[] args) {
+		String baseUrl;
+		String username;
+		String password;
+		AuthType authenticationType;
 		
-		JsonObject jsonObject = new Request("http://jira.domain.com", "username", "password", AuthType.BASIC_AUTH).getJiraAllIssues();
+		if (args.length < 3) {
+			System.out.println("Too few parameters Please provide the URL to Jira, the username and password");
+			System.out.println("e.g. java App.java http://jira.domain.com username password [d]");
+			System.out.println("Where 'd' is an optional number: 1 = Basic Jira Authentication, 2 = Cookie Jira Authentication");
+			System.exit(-1);
+		}
+		
+		//TODO sanitization of strings/URLs and format validation
+		baseUrl = args[0];
+		username = args[1];
+		password = args[2];
+		
+		int auth = args[3] == null ? 0 : Integer.parseInt(args[3]);
+		switch (auth) {
+		case 1:
+			authenticationType = AuthType.BASIC_AUTH;
+			break;
+			
+		case 2:
+			authenticationType = AuthType.COOKIE_BASED_AUTH;
+			break;
+			
+		default:
+			authenticationType = AuthType.BASIC_AUTH;
+			break;
+		}
+		
+		JsonObject jsonObject = new Request(baseUrl, username, password, authenticationType).getJiraAllIssues();
+		jsonObject = postProcessJson(jsonObject);
+		
+		//TODO name the file based on date-time
 		JsonFileManager.writeJsonToFile(jsonObject, "response");
 	}
+
+	private static JsonObject postProcessJson(JsonObject jsonObject) {
+		
+		// TODO return only needed data
+		
+		
+		return jsonObject;
+	}
 }
+
+
+
+
