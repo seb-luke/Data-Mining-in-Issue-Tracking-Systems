a6ca88f8d7c7b0dd6d6f00d35b55154b6cff6109
Sebastian Luca <seb.luke@gmail.com>
2016-07-03 22:08:38 +0300

    Add Javadoc for all methods

--- src/main/java/com/warptronic/itdm/io/JsonFileManager.java
+++ src/main/java/com/warptronic/itdm/io/JsonFileManager.java
@@ -5,8 +5,22 @@ import java.io.IOException;
 
 import javax.json.JsonObject;
 
+/**
+ * A class that only handles serialization of Json Objects and I/O
+ * @author Sebastian Luca
+ *
+ */
 public class JsonFileManager {
+	
+	private JsonFileManager() {
+		//empty constructor as class only has static methods
+	}
 
+	/**
+	 * Writes a JSON to file
+	 * @param jsonObject {@link JsonObject} to be serialized and written to file
+	 * @param filename {@link String} containing the File name (without extension)
+	 */
 	public static void writeJsonToFile(JsonObject jsonObject, String filename) {
 		
 		try (FileWriter file = new FileWriter(filename.concat(".json"))) {
--- src/main/java/com/warptronic/itdm/jira/Request.java
+++ src/main/java/com/warptronic/itdm/jira/Request.java
@@ -1,9 +1,16 @@
 package com.warptronic.itdm.jira;
 
+import java.util.function.Function;
+
 import javax.json.JsonObject;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.*;
 
+/**
+ * This class contains all the methods to communicate with Jira
+ * @author Sebastian Luca
+ *
+ */
 public class Request {
 	private AuthorizationManager authManager;
 	private AuthType authType;
@@ -11,6 +18,13 @@ public class Request {
 	private Client client;
 	private WebTarget jiraRestBaseTarget;
 	
+	/**
+	 * 
+	 * @param jiraBasePath - a {@link String} containing the URL path to the Jira Installation <br><em>(e.g. https://jira.domain.com:8089)</em>
+	 * @param userName - a {@link String} containing the User Name of any user accepted by Jira
+	 * @param password - a {@link String} containing the Password for the provided user
+	 * @param authType - an {@link AuthType} showing what authentication is wanted in communicating with Jira
+	 */
 	public Request(String jiraBasePath, String userName, String password, AuthType authType) {
 		authManager = new AuthorizationManager(userName, password);
 		this.authType = authType;
@@ -19,6 +33,13 @@ public class Request {
 		this.jiraRestBaseTarget = client.target(jiraBasePath).path("rest");
 	}
 	
+	//TODO create a new method to take only 100 items per call if there are too many; check method time for a huge # of issues
+	//TODO create a new method use the above together with a filtering parameter (Method Reference)
+	
+	/**
+	 * The method will return <strong>all</strong> Jira issues. Beware when the issues are more than 1000!
+	 * @return a {@link JsonObject} containing all the issues requested
+	 */
 	public JsonObject getJiraAllIssues() {
 		WebTarget jiraIssueSearch = jiraRestBaseTarget.path("api").path("2").path("search").queryParam("jql", "");
 		
@@ -35,6 +56,11 @@ public class Request {
 		return invocationBuilder.get(JsonObject.class);
 	}
 
+	/**
+	 * This method only returns data regarding one issue from Jira, for the Issue-Key provided
+	 * @param issue as {@link String} containing the Issue Key (Jira Style) <em>ISSUE-210</em>
+	 * @return a {@link JsonObject} containing all the data for the requested issue
+	 */
 	public JsonObject getJiraSpecificIssueBaseAuth(String issue) {
 		WebTarget jiraIssue = jiraRestBaseTarget.path("api").path("2").path("issue");
 		WebTarget currentIssue = jiraIssue.path(issue);
@@ -43,6 +69,17 @@ public class Request {
 		invocationBuilder = authType == AuthType.BASIC_AUTH ? authManager.addBasicAuthorizationHeader(invocationBuilder) : authManager.addCookieAuthHeader(invocationBuilder);
 		
 		return invocationBuilder.get(JsonObject.class);
+	}
+
+	/**
+	 * This method will return only the needed information in the issue for all of the issues from Jira
+	 * @param filteringFunction - a Method Reference ({@link Function}) whose role is to take only the needed information from the provided Issues
+	 * @return a {@link JsonObject} containing the filtered data from all of Jira's Issues
+	 */
+	public JsonObject getFilteredJiraIssues(Function<JsonObject, JsonObject> filteringFunction) {
+		JsonObject json = this.getJiraAllIssues();
+		
+		return filteringFunction.apply(json);
 	}
 	
 
