2952de1d04243d4fd59f247f26d3b24c0f6a9aef
Sebastian Luca <seb.luke@gmail.com>
2016-07-03 22:07:36 +0300

    Added a way to filter the data from Jira using Lambda (Method Reference)

--- src/main/java/com/warptronic/itdm/App.java
+++ src/main/java/com/warptronic/itdm/App.java
@@ -1,14 +1,32 @@
 package com.warptronic.itdm;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
 
 import com.warptronic.itdm.io.JsonFileManager;
 import com.warptronic.itdm.jira.AuthType;
 import com.warptronic.itdm.jira.Request;
 
-
+/**
+ * This is the main class that runs the Jira Issue Exporter
+ * It is here just as an example. I advise you to write your own logic
+ * @author Sebastian Luca
+ *
+ */
 public class App {
 	
+	private App() {
+		//class doesn't need initialization
+	}
+	
+	/**
+	 * 
+	 * @param args arguments provided from command line in order: <code>username password auth_type</code>
+	 */
 	public static void main (String[] args) {
 		String baseUrl;
 		String username;
@@ -42,19 +60,60 @@ public class App {
 			break;
 		}
 		
-		JsonObject jsonObject = new Request(baseUrl, username, password, authenticationType).getJiraAllIssues();
-		jsonObject = postProcessJson(jsonObject);
+		/**
+		 * This is an example showing how to save all Jira issues to file
+		 */
+		 //JsonObject jsonObject = new Request(baseUrl, username, password, authenticationType).getJiraAllIssues();
 		
+		
+		/**
+		 * This is an example showing how to save only the needed data from Jira
+		 */
+		JsonObject jsonObject = new Request(baseUrl, username, password, authenticationType).getFilteredJiraIssues(App::jiraMinimalFilter);
+		
+		/**
+		 * This is an example of how to write the created JSON object to file
+		 */
 		//TODO name the file based on date-time
 		JsonFileManager.writeJsonToFile(jsonObject, "response");
 	}
 
-	private static JsonObject postProcessJson(JsonObject jsonObject) {
+	/**
+	 * <h3>Disclaimer: this is just an example showing how to create a filtering method to be refferenced</h3>
+	 * This is a method referenced to {@link Request#getFilteredJiraIssues(java.util.function.Function)} as a parameter
+	 * The purpose of this method is to filter the JSON received from Jira and save only the needed information 
+	 * @param jsonObject a {@link JsonObject} parameter containing the requested information from Jira
+	 * @return a {@link JsonObject} containing only the needed information
+	 */
+	private static JsonObject jiraMinimalFilter(JsonObject jsonObject) {
+		JsonArrayBuilder jsonBuilder = Json.createArrayBuilder();
 		
-		// TODO return only needed data
+		for (JsonValue j : jsonObject.getJsonArray("issues")) {
+			if (!ValueType.OBJECT.equals(j.getValueType())) {
+				//TODO throw exception
+				System.out.println("expected JsonValue was Object, but was " + j.getValueType());
+				System.exit(-1);
+			} 
+			JsonObject json = (JsonObject) j;
+
+			JsonObjectBuilder jobjBuilder = Json.createObjectBuilder();
+			JsonObject parentKeyObj = json.getJsonObject("fields").getJsonObject("parent");
+			String parentKey = parentKeyObj == null ? "" : parentKeyObj.getString("key");
+			boolean isDone = json.getJsonObject("fields").containsKey("resolutiondate") && !ValueType.NULL.equals(json.getJsonObject("fields").get("resolutiondate").getValueType());
+			String endDate = isDone ? json.getJsonObject("fields").getString("resolutiondate") : "";
+			
+			jobjBuilder.add("key", json.getString("key"))
+						.add("parent-key", parentKey)
+						.add("issuetype", json.getJsonObject("fields").getJsonObject("issuetype").getString("name"))
+						.add("status", json.getJsonObject("fields").getJsonObject("status").getString("name"))
+						.add("start-date", json.getJsonObject("fields").getString("created"))
+						.add("end-date", endDate);
+
+			jsonBuilder.add(jobjBuilder);
+		}
 		
-		
-		return jsonObject;
+		return Json.createObjectBuilder().add("total", jsonObject.getInt("total"))
+										.add("issues", jsonBuilder).build();
 	}
 }
 
