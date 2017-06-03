45ed0fedfd2701baba8deb6a201ef804682446c0
Sebastian Luca <seb.luke@gmail.com>
2017-06-03 16:51:03 +0300

    Implemented UrlUtils.isUrl

--- src/main/java/com/warptronic/itdm/utils/UrlUtils.java
+++ src/main/java/com/warptronic/itdm/utils/UrlUtils.java
@@ -1,13 +1,16 @@
 package com.warptronic.itdm.utils;
 
+import org.apache.commons.validator.routines.UrlValidator;
 import org.glassfish.jersey.Beta;
 
 public class UrlUtils {
+	
+	private UrlUtils() {}
 
 	@Beta
 	public static boolean isUrl(String url) {
-		// TODO check if parameter is indeed a well-formated URL
-		return true;
+		
+		return new UrlValidator(new String[] {"http", "https"}).isValid(url);
 	}
 
 }
